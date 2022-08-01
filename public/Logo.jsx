f0ffa3af83f41c4a88fc35b18d6b2ee08f3c1c25
olamiji-8 <olamijisotomi@gmail.com>
2022-08-02 00:57:35 +0100

    logo

---
+++ public/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div>
+        <h3 style={{color: "lightblue"}}>OLAPELU</h3>
+    </div>
+  )
+}
+
+export default Logo
---
+++ src/page/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from '../../../public/logo'
+
+const Home = () => {
+  return (
+    <div>
+        <Logo/>
+        hi
+    </div>
+  )
+}
+
+export default Home
