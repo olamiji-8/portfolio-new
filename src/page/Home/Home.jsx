cb8a758b37c4e3f96953845f5f342e68c768ddc8
olamiji-8 <olamijisotomi@gmail.com>
2022-08-02 11:40:27 +0100

    update

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -3,6 +3,10 @@ import NavBar from '../../component/NavBar/NavBar'
 import About from '../../container/About/About'
 import Contact from '../../container/Contact/Contact'
 import Logo from '../../Logo'
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import ContactMailIcon from '@mui/icons-material/ContactMail';
+import PersonIcon from '@mui/icons-material/Person';
 
 const Home = () => {
   return (
@@ -16,10 +20,15 @@ const Home = () => {
             <h5>I’m focused on building responsive Frontend web applications while learning & exploring other technologies.</h5>
 
         </div>
+        <LinkedInIcon/>
+        <GitHubIcon/>
+        <ContactMailIcon/>
+        <PersonIcon/>
         <About/>
         <Contact/>
+        
     </div>
   )
 }
 
-export default Home
+export default Home;
