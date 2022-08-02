9982a2a92690f99823bb53dc2df881925f13f978
olamiji-8 <olamijisotomi@gmail.com>
2022-08-02 02:03:27 +0100

    about

---
+++ src/container/About/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h3>ABOUT</h3>
+        <h2>Who AM I</h2>
+        <div>
+            <h5>Hi I am Sotomi Olamiji</h5>
+            <p><h5>Well, I have been working on web applications, which I started with Htnl, Css and Javascript and down to react.</h5></p>
+            <img src='' alt=''/>
+        </div>
+
+        <button>Check out my project</button>
+
+    </div>
+  )
+}
+
+export default About
