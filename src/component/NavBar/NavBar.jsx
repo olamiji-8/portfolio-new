34eb109b7a0e35c1738874642b72ce0084c2a2f2
olamiji-8 <olamijisotomi@gmail.com>
2022-08-02 01:09:46 +0100

    navbar

---
+++ src/component/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+        <h3>About</h3>
+        <h3>Work</h3>
+        <h3>Skills</h3>
+        <h3>COntact</h3>
+        <button>Resume</button>
+    </div>
+  )
+}
+
+export default NavBar
