488c5023eba73075acad1a972040f178d21961c9
olamiji-8 <olamijisotomi@gmail.com>
2022-08-05 01:40:01 +0100

    update

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import './About.css'
 
 const About = () => {
   return (
-    <div>
+    <div className='About'>
         <h3>ABOUT</h3>
         <h2>Who AM I</h2>
-        <div>
+        <div className=''>
             <h5>Hi I am Sotomi Olamiji</h5>
             <p><h5>Well, I have been working on web applications, which I started with Htnl, Css and Javascript and down to react.</h5></p>
             <img src='' alt=''/>
