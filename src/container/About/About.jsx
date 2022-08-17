6883abe548786313c0d5c9ef7e1b2153fa781647
olamiji-8 <olamijisotomi@gmail.com>
2022-08-17 20:59:29 +0100

    About

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -5,21 +5,24 @@ import './About.css'
 const About = () => {
   return (
     <div className='About'>
-      <div className='ola'>
-        <img src={pic} alt=''/>
-      </div>
-      <div>
       <h3>ABOUT</h3>
-        <h2>Who AM I</h2>
+      <h2>Who AM I</h2>
+      <div>
+        <span className='ola'>
+        <img src={pic} alt=''/>
+        </span>
+      <span className='details'>
             <h5>Hi I am Sotomi Olamiji</h5>
             <p><h5>Well, I have been working on web applications, which I started with Htnl, Css and Javascript and down to react.
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt quisquam quod harum, molestias commodi enim natus, ipsum architecto voluptatum ad atque modi? Tempore dolorem error cumque quos asperiores quod ullam?
               </h5></p>
-      </div>
+      </span>
         
-            
-
-        <button>Check out my project</button>
+      </div>  
+        <div className='btn-about'>
+          <button>Check out my project</button>
+        </div>  
+        
     </div>
   )
 }
