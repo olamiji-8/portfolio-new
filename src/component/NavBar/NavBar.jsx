67c4bd44e82aa402e62cb4f8767b7d74c1bc7986
olamiji-8 <olamijisotomi@gmail.com>
2022-08-21 06:53:04 +0100

    update

--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
         <h5>Work</h5>
         <h5>Skills</h5>
         <h5>Contact</h5>
-        <button>Resume</button>
+        <button><a href ="https://drive.google.com/file/d/1Mcc7SqLUmtwuloIf9nDhigU-bkKICDGG/view?usp=sharing"></a>Resume</button>
     </div>
   )
 }
