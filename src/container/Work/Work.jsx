fd0c846d730082719ab24d69a5d3eeecdd8c60db
olamiji-8 <olamijisotomi@gmail.com>
2022-08-21 23:00:15 +0100

    update

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -3,23 +3,25 @@ import Enaira from '../../assets/E-naira.png';
 import portfolio  from'../../assets/portfolio.png';
 import blog from'../../assets/blog.png';
 import Airbnb from '../../assets/Airbnb.png';
-import "./Work.css"
+import './Work.css'
 
 const Work = () => {
   return (
     <div className='Work'>
-       <div className='Enaira'>
-        <img src={Enaira} alt=''/>
-       </div>
-       <div className='portfolio'>
-        <img src={portfolio} alt=''/>
-       </div>
-       <div className='blog'>
-        <img src={blog} alt=''/>
-       </div>
-       <div className='Airbnb'>
-        <img src={Airbnb} alt=''/>
-       </div>
+       <div className='project'>
+          <div className='Enaira'>
+          <img src={Enaira} alt=''/>
+          </div>
+          <div className='portfolio'>
+          <img src={portfolio} alt=''/>
+          </div>
+          <div className='blog'>
+          <img src={blog} alt=''/>
+          </div>
+          <div className='Airbnb'>
+          <img src={Airbnb} alt=''/>
+          </div>
+        </div>
     </div>
   )
 }
