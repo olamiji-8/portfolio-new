c6db117454f53d6ec72a0e3cef3c4d61bd67c206
olamiji-8 <olamijisotomi@gmail.com>
2022-08-21 22:36:44 +0100

    work

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
+import Enaira from '../../assets/E-naira.png';
+import portfolio  from'../../assets/portfolio.png';
+import blog from'../../assets/blog.png';
+import Airbnb from '../../assets/Airbnb.png';
 import "./Work.css"
 
 const Work = () => {
   return (
     <div className='Work'>
-        <p>
-            uuu
-        </p>
+       <div className='Enaira'>
+        <img src={Enaira} alt=''/>
+       </div>
+       <div className='portfolio'>
+        <img src={portfolio} alt=''/>
+       </div>
+       <div className='blog'>
+        <img src={blog} alt=''/>
+       </div>
+       <div className='Airbnb'>
+        <img src={Airbnb} alt=''/>
+       </div>
     </div>
   )
 }
