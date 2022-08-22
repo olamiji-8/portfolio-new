cecf4f3a44105c94f02b8ba0d6054e63913f38d8
olamiji-8 <olamijisotomi@gmail.com>
2022-08-22 02:22:38 +0100

    Router

--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -1,3 +1,4 @@
+import { Link } from '@mui/material'
 import React from 'react'
 import './NavBar.css'
 
@@ -8,9 +9,12 @@ const NavBar = () => {
         <h5>Work</h5>
         <h5>Skills</h5>
         <h5>Contact</h5>
-        <button><a href ="https://drive.google.com/file/d/1Mcc7SqLUmtwuloIf9nDhigU-bkKICDGG/view?usp=sharing"></a>Resume</button>
+        <Link>
+        <button>Resume</button>
+        </Link>
+        
     </div>
   )
 }
-
+<a href ="https://drive.google.com/file/d/1Mcc7SqLUmtwuloIf9nDhigU-bkKICDGG/view?usp=sharing"></a>
 export default NavBar
