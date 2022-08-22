506dd0c71ee146db0aee8f541a596ffcecdd8f15
olamiji-8 <olamijisotomi@gmail.com>
2022-08-22 21:56:29 +0100

    Links

--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -2,17 +2,23 @@
 import React from 'react'
 import './NavBar.css'
 import resume from '../../assets/Resume.pdf'
-import { Link } from 'react-router-dom'
-import About from '../../container/About/About'
+import {BrowserRouter as Router,Link} from 'react-router-dom';
+
 
 const NavBar = () => {
   return (
     <div className='NavBar'>
-      <a href={'{About}'}><h5>About</h5></a>
+      <Router>
+            <Link><h5>About</h5></Link>
+            <Link><h5>Work</h5></Link>
+            <Link><h5>Skills</h5></Link>
+            <Link><h5>Contact</h5></Link>
+      </Router>
+     
         
-        <h5>Work</h5>
-        <h5>Skills</h5>
-        <h5>Contact</h5>
+        
+        
+        
           <a href={resume}><button>Resume</button></a>
         
           
