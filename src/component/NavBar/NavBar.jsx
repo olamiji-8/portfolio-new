c83a28cfb5e43ea840c54b4fe9d8321c3258c5e1
olamiji-8 <olamijisotomi@gmail.com>
2022-08-22 22:27:38 +0100

    update

--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -9,10 +9,14 @@ const NavBar = () => {
   return (
     <div className='NavBar'>
       <Router>
-            <Link><h5>About</h5></Link>
-            <Link><h5>Work</h5></Link>
-            <Link><h5>Skills</h5></Link>
-            <Link><h5>Contact</h5></Link>
+            <Link to= '/about'><h5>About</h5></Link>
+            <Link to= '/work'><h5>Work</h5></Link>
+            <Link to= '/skills'><h5>Skills</h5></Link>
+            <Link to= '/contact'><h5>Contact</h5></Link>
+
+
+
+            
       </Router>
      
         
