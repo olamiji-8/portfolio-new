22357cf2ee06d8ba1a842a2b8fb53816adf039c2
olamiji-8 <olamijisotomi@gmail.com>
2022-08-24 22:36:48 +0100

    update

--- src/component/Icon/Icon.jsx
+++ src/component/Icon/Icon.jsx
@@ -4,15 +4,16 @@ import LinkedIn from '../../assets/linkedIn.png';
 import GitHub from '../../assets/github.png';
 import Message from '../../assets/message.png';
 import Person from '../../assets/person.png';
+import Resume from '../../assets/Resume.pdf';
 
 
 const Icon = () => {
   return (
     <div className='Icon'>
         <a href='https://www.linkedin.com/in/olamiji-sotomi-9605171a9/'><img src={LinkedIn} alt="" className='imgs'/></a>
-        <img src={GitHub} alt="" className='imgs'/>
-        <img src={Message} alt="" className='imgs'/>
-        <img src={Person} alt="" className='imgs'/>
+        <a href='https://github.com/olamiji-8'><img src={GitHub} alt="" className='imgs'/></a>
+        <a href='#contact'><img src={Message} alt="" className='imgs'/></a>
+        <a href={Resume}><img src={Person} alt="" className='imgs'/></a>
     </div>
   )
 }
