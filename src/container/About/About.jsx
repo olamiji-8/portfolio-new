598425a8504d9e7146648058f21e598cb4c934da
olamiji-8 <olamijisotomi@gmail.com>
2022-08-24 23:27:25 +0100

    update

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -24,12 +24,9 @@ const About = () => {
             while still studying at college. Having my own projects, coded mostly in React JS, I have learned a lot while working on these projects. Now I am looking to get a job or an internship to get some experience, and am glad to be interviewing with you here.
               </h4></p>
               <h4>These are some qualities that I think sets me apart from other candidates</h4>
-              <span>
-              <ArrowLeftIcon/><h4 className='hone'>One of the things that distinguish me from other candidates is that I am able to remain calm and focused no matter how difficult the task. I always try to stay positive and find a way out. I am sure that everything happens for a reason and that any mistake can be fixed.</h4>
-              </span>
-              
-              <ArrowLeftIcon/><h4 className='hone'>I am a quick learner and constantly improve my skills not only in professional but also in multiple other fields. If hired for the position, I will continue to learn and transfer obtained knowledge and skills to other team members.</h4>
-              <ArrowLeftIcon/><h4 className='hone'>You will never find a more disciplined person. I am never late and know how to value the time of other people. Especially in the engineering field, where everything should be clear, structured, and meet requirements.</h4>
+              <h4 className='hone'>* One of the things that distinguish me from other candidates is that I am able to remain calm and focused no matter how difficult the task. I always try to stay positive and find a way out. I am sure that everything happens for a reason and that any mistake can be fixed.</h4>
+              <h4 className='hone'> * I am a quick learner and constantly improve my skills not only in professional but also in multiple other fields. If hired for the position, I will continue to learn and transfer obtained knowledge and skills to other team members.</h4>
+              <h4 className='hone'> * You will never find a more disciplined person. I am never late and know how to value the time of other people. Especially in the engineering field, where everything should be clear, structured, and meet requirements.</h4>
       </span>
         
       </div>  
