3577d60e164c028cb48250dce9300d3c793361e8
olamiji-8 <olamijisotomi@gmail.com>
2022-08-24 18:23:00 +0100

    update

--- src/component/Work/Imgas.js
+++ src/component/Work/Imgas.js
@@ -15,13 +15,18 @@ const Imgas = () => {
           </div>
           <div className='timage'>
           <img src={portfolio} alt=''/>
+          <h3>Portfolio</h3>
           <h5>This is a web application with all information about me and my projet.</h5>
           </div>
           <div className='timage'>
           <img src={blog} alt=''/>
+          <h3>Blog</h3>
+          <h5>This is a blog website, where articles are posted.</h5>
           </div>
           <div className='timage'>
           <img src={Airbnb} alt=''/>
+          <h3>Clone-Airbnb</h3>
+          <h5>This is a clone Airbnb project</h5>
           </div>
     </div>
   )
