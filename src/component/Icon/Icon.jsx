7099e69f6f16c4e6ed8744e0f8b56bb65848c470
olamiji-8 <olamijisotomi@gmail.com>
2022-08-25 21:07:22 +0100

    update

--- src/component/Icon/Icon.jsx
+++ src/component/Icon/Icon.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import './Icon.css'
+import './Icon.css'
 import LinkedIn from '../../assets/linkedIn.png';
 import GitHub from '../../assets/github.png';
 import Message from '../../assets/message.png';
@@ -11,18 +11,18 @@ const Icon = () => {
   return (
     <div className="container">
       <div className="row justify-content-center">
-
+      
       <div className="col-2">
-      <a href='https://www.linkedin.com/in/olamiji-sotomi-9605171a9/'><img src={LinkedIn} alt="" className='' width="40%" /></a>
+      <a href='https://www.linkedin.com/in/olamiji-sotomi-9605171a9/'><img src={LinkedIn} alt="" className='imgs' width="40%" /></a>
       </div>
       <div className="col-2">
-      <a href='https://github.com/olamiji-8'><img src={GitHub} alt="" className='' width="40%" /></a>
+      <a href='https://github.com/olamiji-8'><img src={GitHub} alt="" className='imgs' width="40%" /></a>
       </div>
       <div className="col-2">
-      <a href='#contact'><img src={Message} alt="" className='' width="40%" /></a>
+      <a href='#contact'><img src={Message} alt="" className='imgs' width="40%" /></a>
       </div>
       <div className="col-2">
-      <a href={Resume}><img src={Person} alt="" className='' width="40%" /></a>
+      <a href={Resume}><img src={Person} alt="" className='imgs' width="40%" /></a>
       </div>
       </div>
     </div>
