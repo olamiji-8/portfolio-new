83a84e7e9b89b48acc50ed42d58e8c5db2d465dd
olamiji-8 <olamijisotomi@gmail.com>
2022-08-25 22:26:19 +0100

    update

--- src/component/Work/Imgas.js
+++ src/component/Work/Imgas.js
@@ -15,23 +15,28 @@ const project = [
   {
     title:"Portfolio",
     img:portfolio ,
-    content:"This is a web application with all information about me and my projet."
+    content:"This is a web application with all information about me and my projet.",
+    link:"http://portfolio-new-hazel.vercel.app/"
+    
   },
   {
     title:"Clone-Airbnb",
     img:Airbnb ,
-    content:"This is a clone Airbnb project"
+    content:"This is a clone Airbnb project",
+    link:"https://clone-airbnb-pi.vercel.app/"
   },
   {
     title:"Blog",
     img:blog ,
-    content:"This is a blog website, where articles are posted."
+    content:"This is a blog website, where articles are posted.",
+    link:"https://blog-three-tan-43.vercel.app/"
   },
-  {
-    title:"ZeroGrid",
-    img:Enaira ,
-    content:"This is a website created during E-naira hackathon,to improve the usage of E-naira"
-  },
+  // {
+  //   title:"ZeroGrid",
+  //   img:Enaira ,
+  //   content:"This is a website created during E-naira hackathon,to improve the usage of E-naira",
+  //   link:""
+  // },
 ]
 
 
@@ -44,7 +49,7 @@ const project = [
 
 
     <Row xs={1} md={3} className="g-4">
-      {project.map(({title, img, content}, idx) => (
+      {project.map(({title, img, content,link}, idx) => (
         <Col>
           <Card>
             <Card.Img variant="top" src={img} />
@@ -53,7 +58,7 @@ const project = [
               <Card.Text>
                {content}
               </Card.Text>
-              <a href='#'>Link here</a>
+              <a href={link}>Link here</a>
             </Card.Body>
           </Card>
         </Col>
