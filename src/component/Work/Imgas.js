ac0d3fe36afed7186dddf60783c4736a8cf96e8e
olamiji-8 <olamijisotomi@gmail.com>
2022-08-25 22:09:10 +0100

    update

--- src/component/Work/Imgas.js
+++ src/component/Work/Imgas.js
@@ -18,14 +18,14 @@ const project = [
     content:"This is a web application with all information about me and my projet."
   },
   {
-    title:"ZeroGrid",
-    img:Enaira ,
-    content:"This is a website created during E-naira hackathon,to improve the usage of E-naira"
+    title:"Clone-Airbnb",
+    img:Airbnb ,
+    content:"This is a clone Airbnb project"
   },
   {
-    title:"ZeroGrid",
-    img:Enaira ,
-    content:"This is a website created during E-naira hackathon,to improve the usage of E-naira"
+    title:"Blog",
+    img:blog ,
+    content:"This is a blog website, where articles are posted."
   },
   {
     title:"ZeroGrid",
@@ -38,28 +38,7 @@ const project = [
 
 
   return (
-    // <div className='projects'>
-    //     <div className='timage'>
-    //       <img src={Enaira} alt=''/>
-    //       <h3>ZeroGrid</h3>
-    //       <h5>This is a website created during E-naira hackathon,to improve the usage of E-naira</h5>
-    //       </div>
-    //       <div className='timage'>
-    //       <img src={portfolio} alt=''/>
-    //       <h3>Portfolio</h3>
-    //       <h5>This is a web application with all information about me and my projet.</h5>
-    //       </div>
-    //       <div className='timage'>
-    //       <img src={blog} alt=''/>
-    //       <h3>Blog</h3>
-    //       <h5 className='move'>This is a blog website, where articles are posted.</h5>
-    //       </div>
-    //       <div className='timage'>
-    //       <img src={Airbnb} alt=''/>
-    //       <h3>Clone-Airbnb</h3>
-    //       <h5 className='move' >This is a clone Airbnb project</h5>
-    //       </div>
-    // </div>
+    
     <>
     <div className='container'>
 
