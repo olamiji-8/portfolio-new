7fcf25f2435b388b2d10f8319e066c2c6dc9c985
olamiji-8 <olamijisotomi@gmail.com>
2023-04-05 00:49:03 +0100

    update

--- src/component/Skills/Images.js
+++ src/component/Skills/Images.js
@@ -1,9 +1,7 @@
 import React from "react";
 // import '../../component/Skills/Images.css'
-import dart from '../../../src/assets/dart.png';
+
 import firebase from '../../../src/assets/firebase.png';
-import flutter from '../../../src/assets/flutter.png';
-import python from '../../../src/assets/python.png';
 import react from '../../../src/assets/react.png';
 import html from '../../../src/assets/html.png';
 import css from '../../../src/assets/css.png';
@@ -13,18 +11,9 @@ import js from '../../../src/assets/js.png';
 const Images = () => {
   return (
     <div className="row justify-content-center mt-4">
-         <div className="col-md-2 col-4">
-          <img src={dart} alt='' width="30%" className="Image" />
-          <h3>Dart</h3>
-          </div>
-        <div className="col-md-2 col-4">
-          <img src={flutter} alt='' width="30%" className="Image"/>
-          <h3>Flutter</h3>
-          </div>
-        <div className="col-md-2 col-4">
-          <img src={python} alt='' width="30%" className="Image"/>
-          <h3>Python</h3>
-          </div>
+        
+       
+        
         <div className="col-md-2 col-4">
           <img src={react} alt='' width="30%" className="Image"/>
           <h3>React</h3>
